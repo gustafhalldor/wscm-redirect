539f54001c2d36a9ae0cd97758dfd615ea29b2a1
gustafhalldor <gustafh@gmail.com>
2018-03-01 14:00:03 +0000

    Added 'back' button to PaymentPage

--- client/src/PaymentPage/PaymentPage.js
+++ client/src/PaymentPage/PaymentPage.js
@@ -29,6 +29,10 @@ class PaymentPage extends Component {
     this.props.updateCcDetails(target);
   };
 
+  onBackButtonClick = () => {
+    this.props.history.push(`/${this.props.match.params.redirectkey}/deliveryOptions`);
+  };
+
   handleConfirmClick = () => {
     if (this.props.created) {
       toast('Sending hefur nú þegar verið búin til !', { type: 'warning' });
@@ -72,6 +76,8 @@ class PaymentPage extends Component {
 
           this.props.changeCreatedStatus(true);
 
+          //TODO: EYÐA út úr localstorage
+
           // Flag shipment as 'CREATED'
           const url2 = `http://localhost:8989/wscm/landing/${this.props.match.params.redirectkey}`;
           const myHeaders2 = new Headers();
@@ -104,79 +110,116 @@ class PaymentPage extends Component {
     const { number, name, expiry, cvc, focused } = this.props.ccDetails;
     return (
       <div className="paymentPageContainer">
-        <div className="paymentPageLeftSide">
-          <div className="flex-container-column costReview">
-            <h3>Kostnaður:</h3>
-            <span>Vörur: {this.props.basketPrice} kr.</span>
-            <span>Sendingarmáti: {this.props.selectedOption.price} kr.</span>
-            <span>Samtals: {this.props.basketPrice + this.props.selectedOption.price} kr.</span>
+        <div className="paymentPageContent">
+          <div className="paymentPageLeftSide">
+            <div className="flex-container-column costReview">
+              <h3>Kostnaður:</h3>
+          {/*    <div className="table-responsive">  */}
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td>Vörur:</td>
+                      <td>{this.props.basketPrice} kr.</td>
+                    </tr>
+                    <tr>
+                      <td>Sendingarmáti:</td>
+                      <td>{this.props.selectedOption.price} kr.</td>
+                    </tr>
+                    <tr>
+                      <td>Samtals:</td>
+                      <td>{this.props.basketPrice + this.props.selectedOption.price} kr.</td>
+                    </tr>
+                  </tbody>
+                </table>
+          {/*    </div> */}
+            </div>
+            <div className="flex-container-column recipientReview">
+              <h3>Berist til:</h3>
+          {/*     <div className="table-responsive">  */}
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td>Nafn:</td>
+                      <td>{this.props.customer.fullName} langalangalanganafnið</td>
+                    </tr>
+                    <tr>
+                      <td>Heimilisfang:</td>
+                      <td>{this.props.customer.address}</td>
+                    </tr>
+                    <tr>
+                      <td>Póstnúmer:</td>
+                      <td>{this.props.customer.postcode}</td>
+                    </tr>
+                    <tr>
+                      <td>Land:</td>
+                      <td>{this.props.selectedCountry}</td>
+                    </tr>
+                  </tbody>
+                </table>
+          {/*    </div>  */}
+            </div>
           </div>
-          <div className="flex-container-column recipientReview">
-            <h3>Berist til:</h3>
-            <span>{this.props.customer.fullName}</span>
-            <span>{this.props.customer.address}</span>
-            <span>{this.props.customer.postcode}</span>
-            <span>{this.props.selectedCountry}</span>
+          <div className="paymentPageRightSide">
+            <h3>Greiðsluupplýsingar</h3>
+            <div>
+              <Cards
+                number={number}
+                name={name}
+                expiry={expiry}
+                cvc={cvc}
+                focused={focused}
+              />
+            </div>
+            <div className="creditCardForm ">
+              <form onSubmit={this.onFormSubmit}>
+                <div>
+                  <input
+                    className="ccInputField"
+                    type="tel"
+                    name="number"
+                    placeholder="Card Number"
+                    onKeyUp={this.handleInputChange}
+                    onFocus={this.handleInputFocus}
+                  />
+                </div>
+                <div>
+                  <input
+                    className="ccInputField"
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onKeyUp={this.handleInputChange}
+                    onFocus={this.handleInputFocus}
+                  />
+                </div>
+                <div>
+                  <input
+                    className="ccInputField"
+                    type="tel"
+                    name="expiry"
+                    placeholder="Valid Thru"
+                    onKeyUp={this.handleInputChange}
+                    onFocus={this.handleInputFocus}
+                  />
+                </div>
+                <div>
+                  <input
+                    className="ccInputField"
+                    type="tel"
+                    name="cvc"
+                    placeholder="CVC"
+                    onKeyUp={this.handleInputChange}
+                    onFocus={this.handleInputFocus}
+                  />
+                </div>
+              </form>
+            </div>
+            <ToastContainer />
           </div>
         </div>
-
-        <div className="paymentPageRightSide">
-          <h3>Greiðsluupplýsingar</h3>
-          <div>
-            <Cards
-              number={number}
-              name={name}
-              expiry={expiry}
-              cvc={cvc}
-              focused={focused}
-            />
-          </div>
-          <div className="creditCardForm ">
-            <form onSubmit={this.onFormSubmit}>
-              <div>
-                <input
-                  className="ccInputField"
-                  type="tel"
-                  name="number"
-                  placeholder="Card Number"
-                  onKeyUp={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-              <div>
-                <input
-                  className="ccInputField"
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  onKeyUp={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-              <div>
-                <input
-                  className="ccInputField"
-                  type="tel"
-                  name="expiry"
-                  placeholder="Valid Thru"
-                  onKeyUp={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-              <div>
-                <input
-                  className="ccInputField"
-                  type="tel"
-                  name="cvc"
-                  placeholder="CVC"
-                  onKeyUp={this.handleInputChange}
-                  onFocus={this.handleInputFocus}
-                />
-              </div>
-            </form>
-          </div>
-          <button className="primary" onClick={this.handleConfirmClick}>Ganga frá greiðslu</button>
-          <ToastContainer />
+        <div className="flex-container-row paymentPageButtons">
+          <button className="btn" onClick={this.onBackButtonClick}>Til baka</button>
+          <button className="btn primary" onClick={this.handleConfirmClick}>Ganga frá greiðslu</button>
         </div>
       </div>
     );
